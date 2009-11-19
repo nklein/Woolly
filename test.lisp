58cdd7ea80a0b0c952505e2b36c24b23ef12235f
Patrick Stein <pat@nklein.com>
2009-11-19 10:49:31 -0600

    Tweaked to be a little more attractive.

--- test.lisp
+++ test.lisp
@@ -5,17 +5,17 @@
 
 (defun test ()
   (let ((font (sheeple:object :parents toolkit:=font=
-			      :em-size 18
+			      :em-size 48
 			      :pathname "okolaks/okolaksRegular.ttf")))
     (let ((app (sheeple:object :parents toolkit:=app=))
 	  (win (sheeple:object :parents toolkit:=window=
 			       :title "Woolly Window 1"
-			       :width 320
-			       :height 240))
+			       :width 640
+			       :height 480))
 	  (but (sheeple:object :parents toolkit:=button=
 			       :offset-x 40
 			       :offset-y 40
-			       :width 100
+			       :width 300
 			       :height 100
 			       :font font
 			       :label "Button")))
