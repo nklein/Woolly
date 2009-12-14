f18b5d5dc28b04e6a194757d7ebc1f03bb6ebd4d
Patrick Stein <pat@nklein.com>
2009-12-14 17:49:58 -0600

    Tweaked so one can specify a checked checkbox.

--- woolly/checkbox.lisp
+++ woolly/checkbox.lisp
@@ -4,6 +4,12 @@
 (sheeple:defproto =checkbox= (=button=)
   (pushed))
 
+(sheeple:defreply sheeple:init-object :after ((checkbox =checkbox=)
+					      &key (pushed nil pushed-p)
+					      &allow-other-keys)
+  (when pushed-p
+    (setf (pushed checkbox) pushed)))
+
 (sheeple:defreply clicked ((checkbox =checkbox=) mouse-button xx yy)
   (declare (ignore mouse-button xx yy))
   (setf (pushed checkbox) (not (pushed checkbox)))
