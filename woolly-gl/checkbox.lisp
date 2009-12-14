4138edee15c86b7129745e7e2ca9dfd839326781
Patrick Stein <pat@nklein.com>
2009-12-14 17:30:43 -0600

    Tweaked to make sure color is set before drawing label.

--- woolly-gl/checkbox.lisp
+++ woolly-gl/checkbox.lisp
@@ -11,5 +11,6 @@
       (draw-bevel-box (/ hh 2) (/ hh 2) 5 (not (woolly:pushed checkbox))
 		      '(0.5 0.5 0.5 1) '(0.75 0.75 0.75 1))
       (gl:translate 0 (/ hh -4) 0))
+    (gl:color 0 0 0 1)
     (woolly:draw-string (woolly:font checkbox) (woolly:label checkbox)
 			:xx (* hh 3/4) :yy (/ hh 2) :centered :vertical)))
